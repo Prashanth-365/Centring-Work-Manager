4b40ac8fc12a06fa780f15191c7ab3ed63b2b8d9
Prashanth-365 <Prashanth-365@github.com>
2026-06-17 10:08:51 +0530

    Add CI, PWA assets, base configs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'app.centering.manager',
+  appName: 'Centering Work Manager',
+  webDir: 'dist',
+}
+
+export default config
